2eece49857fb426e055d42c3716e896ea090f952
Leonardo_Sanfins <leonardo.henrique@multiway.com.br>
2022-05-13 09:11:07 -0300

    Lista de produtos funcionando

--- src/main/java/br/com/usf/bar/screen/ListaProdutosGUI.java
+++ src/main/java/br/com/usf/bar/screen/ListaProdutosGUI.java
@@ -1,8 +1,26 @@
 package br.com.usf.bar.screen;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.net.http.HttpClient;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.usf.bar.conexao.Conexao;
+
+import br.com.usf.bar.model.ProdutoModel;
+import br.com.usf.bar.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.ui.Model;
 
 public class ListaProdutosGUI extends JFrame{
     private JTextField produtosSearch;
@@ -17,6 +35,8 @@ public class ListaProdutosGUI extends JFrame{
         this.setSize(450, 400);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+        carregaTabela();
+
         botaoVoltar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -25,5 +45,73 @@ public class ListaProdutosGUI extends JFrame{
                 dispose();
             }
         });
+    }
+
+//    public List<ProdutoModel> listar(){
+//
+//        List<ProdutoModel> listaProdutos = new ArrayList<>();
+//
+//        Connection con = Conexao.getConnection();
+//        PreparedStatement pstm = null;
+//        ResultSet rs = null;
+//
+//        try {
+//            pstm = con.prepareStatement("select * from public.produto_model");
+//            rs = pstm.executeQuery();
+//
+//            while(rs.next()){
+//                ProdutoModel produto = new ProdutoModel();
+//
+//                produto.setId((long) rs.getInt("id"));
+//                produto.setNome(rs.getString("nome"));
+//                produto.setValor((double) rs.getInt("valor"));
+//
+//                listaProdutos.add(produto);
+//            }
+//        } catch (SQLException ErroSql) {
+//            JOptionPane.showMessageDialog(null, "Erro ao listar dados " + ErroSql, "ERRO",
+//                JOptionPane.ERROR);
+//        }
+//        finally {
+//            Conexao.closeConnection(con, pstm, rs);
+//        }
+//
+//        return listaProdutos;
+//    }
+
+    public void carregaTabela(){
+        DefaultTableModel modelo = (DefaultTableModel) produtosTable.getModel();
+        modelo.setNumRows(0);
+
+        modelo.addColumn("id");
+        modelo.addColumn("nome");
+        modelo.addColumn("valor");
+
+        produtosTable.getColumnModel().getColumn(0).setPreferredWidth(20);
+        produtosTable.getColumnModel().getColumn(1).setPreferredWidth(80);
+        produtosTable.getColumnModel().getColumn(2).setPreferredWidth(20);
+
+        try{
+            Connection con = Conexao.getConnection();
+            PreparedStatement pstm;
+            ResultSet rs;
+
+            pstm = con.prepareStatement("select * from public.produto_model;");
+            rs = pstm.executeQuery();
+
+            while(rs.next()){
+                modelo.addRow(new Object[]{
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3)
+                });
+            }
+
+            Conexao.closeConnection(con, pstm, rs);
+
+        } catch(Exception ErroSql) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar a tabela de dados: " + ErroSql,
+                    "ERRO", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
